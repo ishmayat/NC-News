df3edbb831693015eb19d7912c5439f3377d6b52
Ismail <ishmayat@hotmail.com>
2024-08-27 15:34:19 +0100

    task 3 completed

--- __tests__/topic.tests.js
+++ __tests__/topic.tests.js
@@ -57,4 +57,51 @@ describe("GET /api", () => {
         expect(response.body).toEqual(expectedEndPoints);
       });
   });
+  test("400: responds with message when given non-existent endpoint.", () => {
+    return request(app)
+      .get("/api/articles/doesthisevenexist")
+      .expect(400)
+      .then(({ body: { msg } }) => {
+        expect(msg).toBe("Bad Request");
+      });
+  });
+});
+describe("GET /api/articles/:articles_id", () => {
+  test("Responds with the correct article.", () => {
+    return request(app)
+      .get("/api/articles/1")
+      .expect(200)
+      .then((response) => {
+        console.log(response.body, "<---- GET /article");
+        // console.log(response.body.article.created_at, "<---- typeof created_at");
+        const { article } = response.body;
+        expect(article).toMatchObject({
+          article_id: 1,
+          title: "Living in the shadow of a great man",
+          topic: "mitch",
+          author: "butter_bridge",
+          body: "I find this existence challenging",
+          created_at: "2020-07-09T20:11:00.000Z",
+          votes: 100,
+          article_img_url:
+            "https://images.pexels.com/photos/158651/news-newsletter-newspaper-information-158651.jpeg?w=700&h=700",
+        });
+      });
+  });
+  test("400: Bad request, INVALID id", () => {
+    return request(app)
+      .get("/api/articles/doesthisevenexist")
+      .expect(400)
+      .then(({ body: { msg } }) => {
+        expect(msg).toBe("Bad Request");
+      });
+  });
+  test("404: responds with message when given non-existent id.", () => {
+    return request(app)
+      .get("/api/articles/64000")
+      .expect(404)
+      .then(({ body: { msg } }) => {
+        expect(msg).toBe("Not Found");
+      });
+  });
 });
